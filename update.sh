1208b8087e1e07d8f626a568b03cc98a1a5f0307
Nauval Atmaja <nauval.atmaja@gmail.com>
2018-12-27 11:13:48 +0700

    Multi Dockerfile for multi golang version
    Why:
    To support different version of golang
    
    This change addresses the need by:
    Creating update.sh
    
    TODO:
    - Use travis to auto build and push to docker hub

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+versions=(
+    1.9
+    1.10
+    1.11
+)
+
+for version in "${versions[@]}"
+do
+    for variant in \
+		alpine3.{7,8} \
+	; do
+		echo "Updating Dockerfile for Golang $version/$variant"
+		mkdir -p $version/$variant
+	    tag=${variant#alpine} # yields 3.7,3.8...
+	    template='alpine'
+
+		sed \
+			-e 's!%%VERSION%%!'"$version"'!g' \
+			-e 's!%%VARIANT%%!'"$variant"'!g' \
+			"Dockerfile-${template}.template" > "$version/$variant/Dockerfile"
+	done
+done
